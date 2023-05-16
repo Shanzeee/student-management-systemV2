e008b31ca5992754a76a14270c0b55b058ca25b7
Kacper <kacpersjuszb@gmail.com>
2023-05-16 09:10:46 +0200

    first commit

---
+++ src/main/java/com/brvsk/studentmanagementsystemV2/StudentManagementSystemV2Application.java
@@ -0,0 +1,13 @@
+package com.brvsk.studentmanagementsystemV2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementSystemV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentManagementSystemV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/brvsk/studentmanagementsystemV2/StudentManagementSystemV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brvsk.studentmanagementsystemV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentManagementSystemV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
